1e69158370b5c748c6ededdb31dc88d64d4948ad
Kauã Henrique <kauahloliveria15@gmail.com>
2026-03-10 19:39:30 -0300

    Segundo commit

--- ex01.js
+++ ex01.js
@@ -1,5 +1,7 @@
-var nome = " Kauã"; 
-var idade = 20;
-var cidade = "Londrina";
+let nome = " Kauã"; 
+let idade = 20;
+let cidade = "Londrina";
 
 console.log("Meu nome é "+nome+", tenho "+idade+ " anos e moro em "+cidade+".");
+
+
