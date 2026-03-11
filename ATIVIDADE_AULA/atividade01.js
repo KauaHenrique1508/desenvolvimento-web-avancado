e9f82b365939a570016c903023437fb54976c1aa
Kauã Henrique <kauahloliveria15@gmail.com>
2026-03-10 21:45:58 -0300

    Quinto commit

--- ATIVIDADE_AULA/atividade01.js
+++ ATIVIDADE_AULA/atividade01.js
@@ -7,9 +7,34 @@ const usuarios = [
 ]
 
 // CALCULO DO VALOR TOTAL DE COMPRAS
+console.log("Valor total de compras de cada usuário:")
 for(let usuario of usuarios){
 let calcularTotalCompras = (compras) => {
-    return compras.reduce((total, valor) => total + valor, 0);
+    return compras.reduce((total, valor) => total + valor, 0)
 }
  console.log(calcularTotalCompras(usuario.compras))
 }
+
+//MOSTRAR USUÁRIOS ATIVOS
+console.log("Usuários ativos:") //usuando arrow function
+
+for(let usuario of usuarios){
+    const mostrarUsuariosAtivos = (usuario) => {
+        if(usuario.ativo){
+            console.log(usuario.nome) 
+        }   
+}
+    mostrarUsuariosAtivos(usuario) //faz a chamada da função 
+}
+
+//MOSTRAR MAIORES DE 18 ANOS
+console.log("Usuários maiores de 18 anos:")
+
+const mostrarMaioresDe18 = (usuario) => {
+        for( n in usuario){
+        if(usuario[n].idade > 18){
+            console.log(usuario[n])
+        }
+    }
+}
+mostrarMaioresDe18(usuarios)
